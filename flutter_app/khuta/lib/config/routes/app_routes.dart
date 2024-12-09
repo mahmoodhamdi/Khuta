057289e2ad86c2763413a8eb9abcba3b1c66217e
mahmoodhamdi <hmdy7486@gmail.com>
2024-12-09 19:46:33 +0200

    "Add translations and update wireframes documentation"

---
+++ flutter_app/khuta/lib/config/routes/app_routes.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import '../../core/utils/app_strings.dart';
+
+class Routes {
+  static const String initialRoute = '/';
+  static const String loginRoute = '/login';
+  static const String registerRoute = '/register';
+  static const String homeRoute = '/home';
+}
+
+class AppRoutes {
+  static Route? onGenerateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.initialRoute:
+        return MaterialPageRoute(
+          builder: (context) => Container(), // Replace with Splash Screen
+        );
+      case Routes.loginRoute:
+        return MaterialPageRoute(
+          builder: (context) => Container(), // Replace with Login Screen
+        );
+      case Routes.registerRoute:
+        return MaterialPageRoute(
+          builder: (context) => Container(), // Replace with Register Screen
+        );
+      case Routes.homeRoute:
+        return MaterialPageRoute(
+          builder: (context) => Container(), // Replace with Home Screen
+        );
+      default:
+        return undefinedRoute();
+    }
+  }
+
+  static Route<dynamic> undefinedRoute() {
+    return MaterialPageRoute(
+      builder: ((context) => const Scaffold(
+            body: Center(
+              child: Text(AppStrings.noRouteFound),
+            ),
+          )),
+    );
+  }
+}
---
+++ flutter_app/khuta/lib/injection_container.dart
@@ -0,0 +1,18 @@
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'core/network/network_info.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //! Features
+
+  //! Core
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  //! External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
